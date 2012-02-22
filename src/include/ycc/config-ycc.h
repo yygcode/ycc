cde41cf2b4b25f16712203b3eb5b648984422d8d
yanyg <yygcode@gmail.com>
2012-02-23 01:23:12 +0800

    project config
    ycc global config header files
    
    	- config-os.h
    	  This file includes platform-specific header files depend on
    	  macros from config.h (generated by configure's execution).
    	  This file should not be installed but will be distributed.
    
    	- config-ycc.h
    	  This file includes ycc project specific macros.
    
    	- config-gcc.h
    	  This file includes macros of GNU GCC gcc-compiler's attr.

---
+++ src/include/ycc/config-ycc.h
@@ -0,0 +1,69 @@
+/*
+ * config-ycc.h: project config header file
+ *
+ * Copyright (C) 2012-2013 yanyg (yygcode@gmail.com, cppgp@qq.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING, if not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __YCC_CONFIG_YCC_H_
+#define __YCC_CONFIG_YCC_H_
+
+#ifdef __GNUC__
+#include <ycc/config-gcc.h>
+#endif
+
+#ifndef __aligned
+#define __aligned(x)
+#endif
+
+#ifndef __always_inline
+#define __always_inline inline
+#endif
+
+/*
+ * offsetof(TYPE, MEMBER)
+ *  the offset of MEMBER of structure TYPE */
+#ifndef offsetof
+#ifdef __compiler_offsetof
+#define offsetof(TYPE, MEMBER)	__compiler_offsetof(TYPE,MEMBER)
+#else
+#define offsetof(TYPE, MEMBER)	((size_t) &((TYPE*)0)->MEMBER)
+#endif
+#endif
+
+#ifndef container_of
+#define container_of(ptr, type, member)	\
+	((type*)((char*)ptr - offsetof(type, member)))
+#endif
+
+#ifndef __YCC_UNUSED
+#define __YCC_UNUSED(var)	((void)(var))
+#endif
+
+#ifndef __BEGIN_DECLS
+#ifdef __END_DECLS
+# undef __END_DECLS
+#endif
+#ifdef  __cplusplus
+# define __BEGIN_DECLS  extern "C" {
+# define __END_DECLS    }
+#else
+# define __BEGIN_DECLS
+# define __END_DECLS
+#endif
+#endif
+
+#endif /* __YCC_CONFIG_YCC_H_ */
