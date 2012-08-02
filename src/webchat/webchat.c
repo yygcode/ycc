3830e583f87c41a48764c9c093b5bf6cffe4d921
yanyonggang <yanyonggang@wisdata.com.cn>
2012-08-02 14:34:55 +0800

    webchat initialize

--- src/webchat/webchat.c
+++ src/webchat/webchat.c
@@ -20,7 +20,53 @@
  * <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <locale.h>
+#include <libintl.h>
+
+static struct option const longopts[] = {
+	{"ip", required_argument, NULL, 'i'},
+	{"port", required_argument, NULL, 'p'},
+	{"verbose", optional_argument, NULL, 'v'},
+	{"quiet", no_argument, 'q'},
+	{NULL, 0, NULL, 0},
+};
+#define optstring "i:p:v::q"
+
 int main(int argc, char **argv)
 {
+	int c;
+	u8_t verbose = 2;
+	u16_t port;
+	const char *ip = "";
+
+	setlocale(LC_ALL, "");
+
+	while ((c = getopt_long(argc, argv, optstring, longopts, NULL))
+	       != -1) {
+		switch(c) {
+		case 'i':
+			ip = strdup(optarg);
+			break;
+
+		case 'p':
+			port = (u16_t)atoi(optarg);
+			break;
+
+		case 'v':
+			verbose = optarg ? (u8_t)atoi(optarg) : (verbose + 1);
+			break;
+
+		case 'q':
+			verbose = 0;
+			break;
+
+		default:
+			usage(EXIT_FAILURE);
+		}
+	}
+
+	printf("ip = %s, port = %u, verbose = %u\n", ip, port, verbose);
+
 	return 0;
 }
